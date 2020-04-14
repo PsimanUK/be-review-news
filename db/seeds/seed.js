0b84384c4913baf0c90b7336f4a67de0e4a76620
Simeon Parris <simeonparris@hotmail.com>
2020-04-14 11:00:22 +0100

    Added functionality to export out the appropriate dataset depending on whether working in a development or test environment. Also added the knex file to the .gitignore file and created a 'migrations' folder ready for the next step of the brief.

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -7,7 +7,7 @@ const {
 
 const { formatDates, formatComments, makeRefObj } = require('../utils/utils');
 
-exports.seed = function(knex) {
+exports.seed = function (knex) {
   const topicsInsertions = knex('topics').insert(topicData);
   const usersInsertions = knex('users').insert(userData);
 
