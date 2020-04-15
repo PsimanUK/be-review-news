4f157ccd122393bab2b8dcc667ff130576bfa128
Simeon Parris <simeonparris@hotmail.com>
2020-04-15 14:04:40 +0100

    Added basic error handling functionality for code 500 server errors.

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 
 const apiRouter = require('./routes/api-router')
 
+const { handle500s } = require('./errors/error-handlers');
+
 app.use('/api', apiRouter);
+
+// ERROR HANDLERS
+app.use(handle500s);
+
 
 module.exports = app;
