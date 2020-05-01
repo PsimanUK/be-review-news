7d9e400df9e07cd93ce1819e426840093e357f98
Simeon Parris <simeonparris@hotmail.com>
2020-05-01 08:36:45 +0100

    Made changes to the connection file.

--- connection.js
+++ connection.js
@@ -1,5 +1,10 @@
-const dbConfig = require('./knexfile')
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
+
+const dbConfig =
+    ENV === 'production'
+        ? { client: 'pg', connection: process.env.DATABASE_URL }
+        : require('./knexfile');
 
 module.exports = knex(dbConfig);
 
